e1095ce5399c040c5d394d5843b5a1fb8f45f81d
clarkrowley <clarkdavidrowley@gmail.com>
2021-03-24 15:25:00 -0500

    Partial update for reading JSON files from PathWeaver.

--- src/main/java/frc/robot/auto/BarrelPath.java
+++ src/main/java/frc/robot/auto/BarrelPath.java
@@ -7,144 +7,43 @@ package frc.robot.auto;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.nio.file.Path;
 
-import edu.wpi.first.wpilibj.util.Units;
-import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
-import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 
 public class BarrelPath {
 
   static Trajectory barrelPath;
 
-  public static Trajectory getTrajectory() {
-	return barrelPath;
-  }
-
   public static void init() {
-	var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(DriveConstants.kS,
-        DriveConstants.kV, DriveConstants.kA), DriveConstants.kDriveKinematics, 11);
+		String myPathName = "";
+		String trajectoryfile = "";
 
-    TrajectoryConfig config = new TrajectoryConfig(AutoConstants.MaxSpeedMetersPerSecond, AutoConstants.MaxAccelerationMetersPerSecondSquared)
-    .setKinematics(DriveConstants.kDriveKinematics).addConstraint(autoVoltageConstraint)
-    .addConstraint(new CentripetalAccelerationConstraint(DriveConstants.kmaxCentripetal));
+		myPathName = "barrel";
 
-	/*
-	circlePath = TrajectoryGenerator.generateTrajectory(
-	    new Pose2d(0, 0, new Rotation2d(0)),
-	    List.of(new Translation2d(2, 0),
-	    new Translation2d(2, 2),
-	    new Translation2d(0, 2),
-	    new Translation2d(0, 0),
-	    new Translation2d(2, 0),
-	    new Translation2d(2, 2),
-	    new Translation2d(0, 2),
-	    new Translation2d(0, 0),
-	    new Translation2d(2, 0)),
-	    new Pose2d(2.0, 2.0, new Rotation2d(Units.degreesToRadians(135.))),
-		config);
-		*/
+		trajectoryfile = myPathName + ".wpilib.json";
+		Trajectory trajectory = new Trajectory();
+		try {
+			Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryfile);
+			trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+		} catch (IOException ex) {
+			DriverStation.reportError("Unable to open trajectory: " + trajectoryfile, ex.getStackTrace());
+		}
 
-		/*
-    oldbarrelPath =
-	TrajectoryGenerator.generateTrajectory(
-	    new Pose2d(1., -2.2, new Rotation2d(0)),
-	    List.of(new Translation2d(3.2, -2.2),
-	    new Translation2d(3.8, -2.3),
-	    new Translation2d(4.3, -3.1),
-	    new Translation2d(3.8, -3.6),
-	    new Translation2d(3.4, -2.9),
-	    new Translation2d(4.3, -2.4),
-	    new Translation2d(6.0, -2.1),
-	    new Translation2d(6.8, -1.2),
-	    new Translation2d(6.0, -0.8),
-	    new Translation2d(5.6, -1.5),
-	    new Translation2d(6.2, -2.8),
-	    new Translation2d(7.4, -3.5),
-	    new Translation2d(8.2, -2.8),
-	    new Translation2d(6.4, -2.3),
-	    new Translation2d(4.8, -2.0),
-	    new Translation2d(2.0, -2.0)),
-	    new Pose2d(1.0, -1.9, new Rotation2d(Units.degreesToRadians(180))),
-		config);
-		*/
+		try {
+			trajectoryfile = myPathName + ".txt";
+			Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryfile);
+			FileWriter fileWriter = new FileWriter(trajectoryPath.toString());
+			PrintWriter printWriter = new PrintWriter(fileWriter);
+			printWriter.print(trajectory.toString());
+			printWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		/* clean, 14s
-    barrelPath =
-	TrajectoryGenerator.generateTrajectory(
-		new Pose2d(1., -2.2, new Rotation2d(0)),
-		List.of( new Translation2d(3.2 , -2.2),
-				new Translation2d(3.8 , -2.3),
-				new Translation2d(4.6 , -2.6),
-				new Translation2d(4.8 , -3.1),
-				new Translation2d(4.4 , -3.7),
-				new Translation2d(3.7 , -3.9),
-				new Translation2d(3.2 , -3.6),
-				new Translation2d(3.1 , -2.9),
-				new Translation2d(4.3 , -2.4),
-				new Translation2d(6.1 , -2.3),
-				new Translation2d(7.2 , -1.7),
-				new Translation2d(6.9 , -1),
-				new Translation2d(6   , -.8), 
-				new Translation2d(5.3 , -1),
-				new Translation2d(5.4 , -2),
-				new Translation2d(6.1 , -3),
-				new Translation2d(7.4 , -3.8),
-				new Translation2d(8.3 , -3.6),
-				new Translation2d(8.4 , -2.6),
-				new Translation2d(7.5 , -2.1),
-				new Translation2d(6.5 , -2.1),
-				new Translation2d(4.8 , -2.1)),
-	    new Pose2d(-1.0, -2.2, new Rotation2d(Units.degreesToRadians(180))),
-		config);
-		*/
-
-    barrelPath =
-	TrajectoryGenerator.generateTrajectory(
-		new Pose2d(1., -2.2, new Rotation2d(0)),
-		List.of( new Translation2d(3.2 , -2.2),
-				new Translation2d(3.8 , -2.3),
-				new Translation2d(4.6 , -2.6),
-				new Translation2d(4.7 , -3.1),
-				new Translation2d(4.4 , -3.7),
-				new Translation2d(3.7 , -3.9),
-				new Translation2d(3.2 , -3.6),
-				new Translation2d(3.1 , -2.9),
-				new Translation2d(4.3 , -2.4),
-				new Translation2d(6.1 , -2.3),
-				new Translation2d(7.1 , -1.7),
-				new Translation2d(6.9 , -1),
-				new Translation2d(6   , -.8), 
-				new Translation2d(5.3 , -1),
-				new Translation2d(5.4 , -2),
-				new Translation2d(6.1 , -3),
-				new Translation2d(7.4 , -3.8),
-				new Translation2d(8.3 , -3.6),
-				new Translation2d(8.4 , -2.6),
-				new Translation2d(7.5 , -2.1),
-				new Translation2d(6.5 , -2.1),
-				new Translation2d(4.8 , -2.1)),
-	    new Pose2d(-1.0, -2.2, new Rotation2d(Units.degreesToRadians(180))),
-	    config);
-
-    try {
-      FileWriter fileWriter = new FileWriter("/home/lvuser/barrelPathTrajectory.txt");
-      //FileWriter fileWriter = new FileWriter("/tmp/barrelPathTrajectory.txt");
-      PrintWriter printWriter = new PrintWriter(fileWriter);
-      printWriter.print(barrelPath.toString());
-      printWriter.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-	}
   }
 
 }
---
+++ src/main/java/frc/robot/auto/BouncePaths.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.auto;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+
+public class BouncePaths {
+
+  static Trajectory bouncePath1;
+  static Trajectory bouncePath2;
+  static Trajectory bouncePath3;
+  static Trajectory bouncePath4;
+
+  public static Trajectory getTrajectory(int step) {
+		Trajectory myPath = bouncePath1;
+		switch (step) {
+			case 1:
+			  myPath = bouncePath1;
+			case 2:
+			  myPath = bouncePath2;
+			case 3:
+			  myPath = bouncePath3;
+			case 4:
+			  myPath = bouncePath4;
+		}
+		return myPath;
+  }
+
+  public static void init() {
+		String myPathName = "";
+		String trajectoryfile = "";
+
+		for (int step = 1; step < 5; step++)
+		{
+			switch (step) {
+				case 1:
+					myPathName = "bounce1";
+				case 2:
+					myPathName = "bounce2";
+				case 3:
+					myPathName = "bounce3";
+				case 4:
+					myPathName = "bounce4";
+			}
+
+			trajectoryfile = myPathName + ".wpilib.json";
+			Trajectory trajectory = new Trajectory();
+			try {
+				Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryfile);
+				trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+			} catch (IOException ex) {
+				DriverStation.reportError("Unable to open trajectory: " + trajectoryfile, ex.getStackTrace());
+			}
+
+			try {
+			  trajectoryfile = myPathName + ".txt";
+				Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryfile);
+				FileWriter fileWriter = new FileWriter(trajectoryPath.toString());
+				PrintWriter printWriter = new PrintWriter(fileWriter);
+				printWriter.print(trajectory.toString());
+				printWriter.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+  }
+
+}
